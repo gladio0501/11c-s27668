92fbb0ebf1b132e7cc9be68dcadf755022a2db41
Vladyslav Miekh <107705243+gladio0501@users.noreply.github.com>
2024-03-24 16:37:36 +0100

    Task 1 done

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+#  Task 0.
+# The entire task is to be placed on GitHub (the name of public repository should be in group-number_student-id convention), after each task a commit is to be made to branch lab_3, and at the end a pull request is to be created with at least 1 person from the group invited in it.
+#DONE
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Task 1: List Comprehensions
+# Write a Python program that generates a list of squares of numbers from 1 to 10 using list comprehensions.ares)
+newlist = [x**2 for x in range(1, 11)]
+print("Task 1")
+print(newlist)
+# Task 2: Functions
+# Expand the previous program by defining a function that takes a range of numbers as input and returns a list of squares for that range.e_squares(start, end))
+
+# Task 3: Classes
+# Create a class called SquareGenerator that has a method to generate squares for a given range of numbers.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Task 4: Libraries
+# Utilize the math library to calculate the square root of each number in the generated list from the previous task.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Task 5: Exceptions
+# Handle the case where the end of the range is less than the start in the SquareGenerator class.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# Task 6: Modules
+# Extract the SquareGenerator class into a separate module named square_generator.py.
+
+
+
+# Task 7: Packages
+# Transform the square_generator module into a package by adding an empty __init__.py file and organize it accordingly.
+
+
+# Task 8: Inheritance
+# Create a subclass called CubicGenerator that inherits from the SquareGenerator class. Modify the CubicGenerator to generate cubes instead of squares.
+
+
+# Task 9: Function Overriding
+# Override the square generation method in the Cubic Generator class to generate squares with a check to see if the start of the range is less than the end, if not return an Exceptions
+
+
+# Task 10: Abstract Elements
+# Convert the SquareGenerator class into an abstract base class (ABC) using the abc module, making the generate_squares method abstract. Ensure that the CubicGenerator class implements this abstract method.
+
+
+# Task 11:
+# The result of the assignment should be a link to a pull request.
+
